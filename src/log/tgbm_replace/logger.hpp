1dd5eaafcd311c3d0fcbf2790f06600971b17e2e
Paul <paul.turkovskiy@gmail.com>
2025-10-07 19:09:06 +0300

    update tgbm logger to recent updates

--- src/log/tgbm_replace/logger.hpp
+++ src/log/tgbm_replace/logger.hpp
@@ -2,4 +2,4 @@
 
 #include <log/logger.h>
 
-#define TGBM_LOG(FMT_STR, ...) g_Logger.Log(LogLevelFromTGBMStr(FMT_STR), FMT_STR __VA_OPT__(, ) __VA_ARGS__)
+#define TGBM_LOG(TYPE, FMT_STR, ...) g_Logger.Log(LogLevelFromTGBMStr(#TYPE), FMT_STR __VA_OPT__(, ) __VA_ARGS__)
